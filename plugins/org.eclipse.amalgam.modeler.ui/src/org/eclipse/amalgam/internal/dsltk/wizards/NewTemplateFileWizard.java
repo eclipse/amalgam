431ecb26edc33248ad26737be475febabb641b36
rgronback <rgronback>
2008-09-10 09:03:29 +0000

    Initial contribution

---
+++ plugins/org.eclipse.amalgam.modeler.ui/src/org/eclipse/amalgam/internal/dsltk/wizards/NewTemplateFileWizard.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Borland Software Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Borland Software Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.amalgam.internal.dsltk.wizards;
+
+import org.eclipse.xtend.shared.ui.wizards.NewExtXptResourceWizard;
+
+/**
+ * FIXME workaround for bug 229115 (wizard not exposed in xpand)
+ */
+public class NewTemplateFileWizard extends NewExtXptResourceWizard {
+	
+    public NewTemplateFileWizard() {
+        super("NewTemplateFile.xpt", "xpt", "New Template File", "Creates a new Xpand Template File", null);
+    }
+
+}
