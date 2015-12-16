e7cb7df3fbb0d1fda81fd1a336684ed1aaed655b
cbrun <cedric.brun@obeo.fr>
2015-12-16 16:58:54 +0100

    Refers to the Neon catalog

--- plugins/org.eclipse.amalgam.discovery.modeling/src/org/eclipse/amalgam/discovery/modeling/handlers/ModelingPackageDiscovery.java
+++ plugins/org.eclipse.amalgam.discovery.modeling/src/org/eclipse/amalgam/discovery/modeling/handlers/ModelingPackageDiscovery.java
@@ -35,7 +35,7 @@ import org.eclipse.ui.progress.IProgressService;
 
 public class ModelingPackageDiscovery extends DiscoveryContentProvider {
 
-	private static final String CATALOG_URI = "http://www.eclipse.org/modeling/amalgam/downloads/discovery/mars/modeling.xmi";
+	private static final String CATALOG_URI = "http://www.eclipse.org/modeling/amalgam/downloads/discovery/neon/modeling.xmi";
 
 	@Override
 	public DiscoveryDefinition load(final IProgressMonitor monitor)
