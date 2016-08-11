a0e26a4dd472809aeb09edb61eee1ab9c5e5972d
Faycal Abka <faycal.abka@altran.com>
2016-08-11 11:24:33 +0200

    [487210][Activity Explorer] spaces are inset between Activity Explorer Elements at switch
    Bug: 487210
    Change-Id: I1827688269ef23a71efcb33ed89d0e14f638a333
    Signed-off-by: Faycal Abka <faycal.abka@altran.com>

--- plugins/org.eclipse.amalgam.explorer.activity.ui/src/org/eclipse/amalgam/explorer/activity/ui/api/editor/pages/OverviewActivityExplorerPage.java
+++ plugins/org.eclipse.amalgam.explorer.activity.ui/src/org/eclipse/amalgam/explorer/activity/ui/api/editor/pages/OverviewActivityExplorerPage.java
@@ -42,243 +42,244 @@ import org.eclipse.ui.forms.widgets.TableWrapData;
 import org.eclipse.ui.forms.widgets.TableWrapLayout;
 
 /**
- * Overview page that behaves as a Activity Explorer. That displays navigable links to other pages.
+ * Overview page that behaves as a Activity Explorer. That displays navigable
+ * links to other pages.
  * 
  */
 public class OverviewActivityExplorerPage extends CommonActivityExplorerPage {
 
-  private static final String PAGE_ID = "overview"; //$NON-NLS-1$
+	private static final String PAGE_ID = "overview"; //$NON-NLS-1$
 
-  private static final String PAGE_TITLE = Messages.OverviewActivityExplorerPage_1;
+	private static final String PAGE_TITLE = Messages.OverviewActivityExplorerPage_1;
 
-  class OverviewPageLinkAdapter extends FormTextPageLinkAdapter {
+	class OverviewPageLinkAdapter extends FormTextPageLinkAdapter {
 
-    /**
-     * @param editor
-     */
-    private OverviewPageLinkAdapter(FormEditor editor) {
-      super(editor);
-    }
+		/**
+		 * @param editor
+		 */
+		private OverviewPageLinkAdapter(FormEditor editor) {
+			super(editor);
+		}
 
-    /**
-     * @see org.eclipse.amalgam.explorer.activity.ui.api.actions.util.PageLinkAdapter#linkEntered(org.eclipse.ui.forms.events.HyperlinkEvent)
-     */
-    @SuppressWarnings("synthetic-access")
-    @Override
-    public void linkEntered(HyperlinkEvent hlevent) {
-      super.linkEntered(hlevent);
-      // Replace the original image with the one used in hover mode.
-      updateImage(hlevent, __hoverHrefImageMappings);
-    }
+		/**
+		 * @see org.eclipse.amalgam.explorer.activity.ui.api.actions.util.PageLinkAdapter#linkEntered(org.eclipse.ui.forms.events.HyperlinkEvent)
+		 */
+		@SuppressWarnings("synthetic-access")
+		@Override
+		public void linkEntered(HyperlinkEvent hlevent) {
+			super.linkEntered(hlevent);
+			// Replace the original image with the one used in hover mode.
+			updateImage(hlevent, __hoverHrefImageMappings);
+		}
 
-    /**
-     * @see org.eclipse.amalgam.explorer.activity.ui.api.actions.util.FormTextPageLinkAdapter#linkExited(org.eclipse.ui.forms.events.HyperlinkEvent)
-     */
-    @SuppressWarnings("synthetic-access")
-    @Override
-    public void linkExited(HyperlinkEvent hlevent) {
-      super.linkExited(hlevent);
-      // Restore original image.
-      updateImage(hlevent, __hrefImageMappings);
-    }
+		/**
+		 * @see org.eclipse.amalgam.explorer.activity.ui.api.actions.util.FormTextPageLinkAdapter#linkExited(org.eclipse.ui.forms.events.HyperlinkEvent)
+		 */
+		@SuppressWarnings("synthetic-access")
+		@Override
+		public void linkExited(HyperlinkEvent hlevent) {
+			super.linkExited(hlevent);
+			// Restore original image.
+			updateImage(hlevent, __hrefImageMappings);
+		}
 
-    private void updateImage(HyperlinkEvent hlevent, Map<String, String> imageMappings) {
-      String pageId = (String) hlevent.getHref();
-      // Replace image with the one got from specified map.
-      String image = imageMappings.get(pageId);
-      if (null != image) {
-        ((FormText) hlevent.widget).setImage(pageId, ActivityExplorerActivator.getDefault().getImage(image));
-      }
-    }
-  }
+		private void updateImage(HyperlinkEvent hlevent, Map<String, String> imageMappings) {
+			String pageId = (String) hlevent.getHref();
+			// Replace image with the one got from specified map.
+			String image = imageMappings.get(pageId);
+			if (null != image) {
+				((FormText) hlevent.widget).setImage(pageId, ActivityExplorerActivator.getDefault().getImage(image));
+			}
+		}
+	}
 
-  /**
-   * Normal HREF image mappings.
-   */
-  private static Map<String, String> __hrefImageMappings = new HashMap<String, String>();
+	/**
+	 * Normal HREF image mappings.
+	 */
+	private static Map<String, String> __hrefImageMappings = new HashMap<String, String>();
 
-  /**
-   * Hover HREF image mappings.
-   */
-  private static Map<String, String> __hoverHrefImageMappings = new HashMap<String, String>();
-  
-  
-  private IMenuListener2 listener = new IMenuListener2() {
+	/**
+	 * Hover HREF image mappings.
+	 */
+	private static Map<String, String> __hoverHrefImageMappings = new HashMap<String, String>();
 
-      public void menuAboutToShow(IMenuManager manager) {
-    	  final Session current = ((ActivityExplorerEditorInput) getEditorInput()).getSession();
-    	  Form formWidget = getManagedForm().getForm().getForm();
-        manager.removeAll();
+	private IMenuListener2 listener = new IMenuListener2() {
 
-        for (Session session : SessionManager.INSTANCE.getSessions()) {
-          if (!session.equals(current))
-            formWidget.getMenuManager().add(new OpenActivityExplorerAction2(session));
-        }
+		public void menuAboutToShow(IMenuManager manager) {
+			final Session current = ((ActivityExplorerEditorInput) getEditorInput()).getSession();
+			Form formWidget = getManagedForm().getForm().getForm();
+			manager.removeAll();
 
-      }
+			for (Session session : SessionManager.INSTANCE.getSessions()) {
+				if (!session.equals(current))
+					formWidget.getMenuManager().add(new OpenActivityExplorerAction2(session));
+			}
 
-      public void menuAboutToHide(IMenuManager manager) {
+		}
 
-      }
-      }
-  ;
+		public void menuAboutToHide(IMenuManager manager) {
 
-  public OverviewActivityExplorerPage() {
-    super(null, PAGE_ID, "");
-  }
+		}
+	};
 
-  /**
-   * Constructor.
-   * 
-   * @param editor
-   */
-  public OverviewActivityExplorerPage(FormEditor editor) {
-    super(editor, PAGE_ID, PAGE_TITLE);
-  }
+	public OverviewActivityExplorerPage() {
+		super(null, PAGE_ID, "");
+	}
 
-  @Override
-  protected void createFormContent(IManagedForm managedForm) {
-    super.createFormContent(managedForm);
+	/**
+	 * Constructor.
+	 * 
+	 * @param editor
+	 */
+	public OverviewActivityExplorerPage(FormEditor editor) {
+		super(editor, PAGE_ID, PAGE_TITLE);
+	}
 
-    // Set the header title.
-    ScrolledForm form = managedForm.getForm();
-    form.setText(getHeaderTitle());
-    // Install a default layout.
-    TableWrapLayout layout = new TableWrapLayout();
+	@Override
+	protected void createFormContent(IManagedForm managedForm) {
+		super.createFormContent(managedForm);
 
-    layout.leftMargin = 0;
-    layout.rightMargin = 0;
-    layout.topMargin = 20;
-    layout.bottomMargin = 0;
-    layout.horizontalSpacing = 0;
-    layout.verticalSpacing = 0;
-    layout.numColumns = 2;
+		// Set the header title.
+		ScrolledForm form = managedForm.getForm();
+		form.setText(getHeaderTitle());
+		// Install a default layout.
+		TableWrapLayout layout = new TableWrapLayout();
 
-    Composite body = form.getBody();
-    body.setLayout(layout);
+		layout.leftMargin = 0;
+		layout.rightMargin = 0;
+		layout.topMargin = 20;
+		layout.bottomMargin = 0;
+		layout.horizontalSpacing = 0;
+		layout.verticalSpacing = 0;
+		layout.numColumns = 2;
 
-    // add Session Manager On Fly
+		Composite body = form.getBody();
+		body.setLayout(layout);
 
-    final Form formWidget = managedForm.getForm().getForm();
+		// add Session Manager On Fly
 
-    final Session current = ((ActivityExplorerEditorInput) this.getEditorInput()).getSession();
+		final Form formWidget = managedForm.getForm().getForm();
 
-    // init
-    formWidget.getMenuManager().add(new Separator("empty-list")); //$NON-NLS-1$
+		final Session current = ((ActivityExplorerEditorInput) this.getEditorInput()).getSession();
 
-    // add listener
-    formWidget.getMenuManager().addMenuListener(listener);
+		// init
+		formWidget.getMenuManager().add(new Separator("empty-list")); //$NON-NLS-1$
 
-    // Create the overview content.
+		// add listener
+		formWidget.getMenuManager().addMenuListener(listener);
 
-    // Set image HREF.
-    // FormText richText = null;
+		// Create the overview content.
 
-    for (CommonActivityExplorerPage page : getContributedPages()) {
+		// Set image HREF.
+		// FormText richText = null;
 
-      createSubForm(managedForm, body, page);
+		for (CommonActivityExplorerPage page : getContributedPages()) {
 
-    }
+			createSubForm(managedForm, body, page);
 
-    form.reflow(true);
-  }
-  
-  @Override
+		}
+
+		form.reflow(true);
+	}
+
+	@Override
 	public void dispose() {
-	  	if (getManagedForm() != null) {
-	  		getManagedForm().getForm().getForm().getMenuManager().removeMenuListener(listener);
-	  	}
+		if (getManagedForm() != null) {
+			getManagedForm().getForm().getForm().getMenuManager().removeMenuListener(listener);
+		}
 		super.dispose();
 	}
 
-  protected String getHeaderTitle() {
-    return Messages.OverviewActivityExplorerPage_2 + ((ActivityExplorerEditorInput) getEditorInput()).getName();
-  }
+	protected String getHeaderTitle() {
+		return Messages.OverviewActivityExplorerPage_2 + ((ActivityExplorerEditorInput) getEditorInput()).getName();
+	}
 
-  // public Map<String, Composite> contents = new HashMap<String, Composite>();
+	// public Map<String, Composite> contents = new HashMap<String,
+	// Composite>();
 
-  public Composite createSubForm(IManagedForm managedForm, Composite body, CommonActivityExplorerPage page) {
+	public Composite createSubForm(IManagedForm managedForm, Composite body, CommonActivityExplorerPage page) {
 
-    Composite composite = body;
+		Composite composite = body;
 
-    FormText richText = null;
+		FormText richText = null;
 
-    if (page.isVisible()) {
-      // image
+		if (page.isVisible()) {
+			// image
 
-      String txt = HTMLHelper.imageLinkForm(page);
-      if (!(txt == null || txt.isEmpty())) {
+			String txt = HTMLHelper.imageLinkForm(page);
+			if (!(txt == null || txt.isEmpty())) {
 
-        richText = FormHelper.createRichText(managedForm.getToolkit(), composite, txt, new OverviewPageLinkAdapter(
-            getEditor()));
-        richText.setHyperlinkSettings(managedForm.getToolkit().getHyperlinkGroup());
-        richText.setImage(page.getId(), page.getOverviewImageOff());
+				richText = FormHelper.createRichText(managedForm.getToolkit(), composite, txt,
+						new OverviewPageLinkAdapter(getEditor()));
+				richText.setHyperlinkSettings(managedForm.getToolkit().getHyperlinkGroup());
+				richText.setImage(page.getId(), page.getOverviewImageOff());
 
-        richText.marginHeight = 0;
-        richText.marginWidth = 0;
-        TableWrapData layoutData = new TableWrapData();
-        layoutData.align = TableWrapData.CENTER;
-        layoutData.valign = TableWrapData.MIDDLE;
-        richText.setLayoutData(layoutData);
+				richText.marginHeight = 0;
+				richText.marginWidth = 0;
+				TableWrapData layoutData = new TableWrapData();
+				layoutData.align = TableWrapData.CENTER;
+				layoutData.valign = TableWrapData.MIDDLE;
+				richText.setLayoutData(layoutData);
 
-        txt = HTMLHelper.overviewDescForm(page);
+				txt = HTMLHelper.overviewDescForm(page);
 
-        // Set Layout data.
+				// Set Layout data.
 
-      }
-      // text
+			}
+			// text
 
-      if (!(txt == null || txt.isEmpty())) {
-        ScrolledFormText descriptionForm = new ScrolledFormText(composite, true);
+			if (!(txt == null || txt.isEmpty())) {
+				ScrolledFormText descriptionForm = new ScrolledFormText(composite, true);
 
-        descriptionForm.setBackground(new Color(PlatformUI.getWorkbench().getDisplay(), 255, 255, 255));
+				descriptionForm.setBackground(new Color(PlatformUI.getWorkbench().getDisplay(), 255, 255, 255));
 
-        descriptionForm.setText(txt);
+				descriptionForm.setText(txt);
 
-        descriptionForm.setMinWidth(600);
-        TableWrapData layoutData2 = new TableWrapData();
+				descriptionForm.setMinWidth(600);
+				TableWrapData layoutData2 = new TableWrapData();
 
-        layoutData2.maxHeight = 100;
-        layoutData2.maxWidth = 600;
-        layoutData2.align = TableWrapData.CENTER;
-        layoutData2.valign = TableWrapData.MIDDLE;
-        descriptionForm.setLayoutData(layoutData2);
+				layoutData2.maxHeight = 100;
+				layoutData2.maxWidth = 600;
+				layoutData2.align = TableWrapData.CENTER;
+				layoutData2.valign = TableWrapData.MIDDLE;
+				descriptionForm.setLayoutData(layoutData2);
+				
+				descriptionForm.pack(false);
+			}
 
-      }
+			richText.marginHeight = 0;
+			richText.marginWidth = 40;
+		}
+		return composite;
+	}
 
-      richText.marginHeight = 0;
-      richText.marginWidth = 40;
-    }
-    return composite;
-  }
+	// public Composite getSubFormComposite(final String id) {
+	// return contents.get(id);
+	// }
 
-  // public Composite getSubFormComposite(final String id) {
-  // return contents.get(id);
-  // }
+	@Override
+	public boolean isVisible() {
+		// By default all Overview page are visible (cf. bug 485652)
+		boolean result = true;
+		if (predicate != null) {
+			result &= predicate.isOk();
+		}
+		return result;
+	}
 
-  @Override
-  public boolean isVisible() {
-	//By default all Overview page are visible (cf. bug 485652)
-	 boolean result = true;
-	 if (predicate != null) {
-	     result &= predicate.isOk();
-	 }
-	 return result;
-  }
+	private List<CommonActivityExplorerPage> getContributedPages() {
+		List<CommonActivityExplorerPage> contributed = new ArrayList<CommonActivityExplorerPage>();
+		for (IFormPage page : ((ActivityExplorerEditor) getEditor()).getPages()) {
+			if (page instanceof CommonActivityExplorerPage) {
+				CommonActivityExplorerPage ap = (CommonActivityExplorerPage) page;
+				if (ap.contributeToOverview()) {
+					contributed.add(ap);
+					__hrefImageMappings.put(ap.getId(), ap.getOverViewImageOffPath());
+					__hoverHrefImageMappings.put(ap.getId(), ap.getOverViewImageOnPath());
+				}
+			}
 
-  private List<CommonActivityExplorerPage> getContributedPages() {
-    List<CommonActivityExplorerPage> contributed = new ArrayList<CommonActivityExplorerPage>();
-    for (IFormPage page : ((ActivityExplorerEditor) getEditor()).getPages()) {
-      if (page instanceof CommonActivityExplorerPage) {
-        CommonActivityExplorerPage ap = (CommonActivityExplorerPage) page;
-        if (ap.contributeToOverview()) {
-          contributed.add(ap);
-          __hrefImageMappings.put(ap.getId(), ap.getOverViewImageOffPath());
-          __hoverHrefImageMappings.put(ap.getId(), ap.getOverViewImageOnPath());
-        }
-      }
-
-    }
-    return contributed;
-  }
+		}
+		return contributed;
+	}
 }
