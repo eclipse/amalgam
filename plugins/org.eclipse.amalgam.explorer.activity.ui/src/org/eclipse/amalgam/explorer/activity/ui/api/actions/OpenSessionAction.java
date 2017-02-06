f54d30188a33bbbf5dc02caec2a4335eda280bc7
Faycal Abka <faycal.abka@gmail.com>
2017-02-06 09:37:58 +0100

    [510762][Activity Explorer] Open action doesn't open the Activity Explorer Editor when a diagram editor is opened
    Bug:510762
    Change-Id: I137d60960b411d9f50e15ab8559e9ce4c87aa55e
    Signed-off-by: Faycal Abka <faycal.abka@gmail.com>

--- plugins/org.eclipse.amalgam.explorer.activity.ui/src/org/eclipse/amalgam/explorer/activity/ui/api/actions/OpenSessionAction.java
+++ plugins/org.eclipse.amalgam.explorer.activity.ui/src/org/eclipse/amalgam/explorer/activity/ui/api/actions/OpenSessionAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c)  2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c)  2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -201,8 +201,7 @@ public class OpenSessionAction extends BaseSelectionListenerAction {
 								SessionHelper.getFirstAnalysisFile((DAnalysisSession)session);
 						ActivityExplorerEditorInput input = new ActivityExplorerEditorInput(file);
 
-						
-						activePage.openEditor(input, ActivityExplorerEditor.ID);
+						activePage.openEditor(input, ActivityExplorerEditor.ID, true, IWorkbenchPage.MATCH_ID | IWorkbenchPage.MATCH_INPUT);
 						welcomeOpen[0] = true;
 					}
 				} catch (PartInitException exception) {
