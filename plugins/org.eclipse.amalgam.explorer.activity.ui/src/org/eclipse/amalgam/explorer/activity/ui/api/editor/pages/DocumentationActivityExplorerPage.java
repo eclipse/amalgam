cb6ded54ff9ff84e78c2cf9b8119d93d80bf5377
Pierre-Charles David <pierre-charles.david@obeo.fr>
2017-08-02 15:22:13 +0200

    [cleanup] Cleanup sources of DocumentationActivityExplorerPage
    Change-Id: If492c1770d6fc77f4595f437b8bfd4f6e3efcdac
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.amalgam.explorer.activity.ui/src/org/eclipse/amalgam/explorer/activity/ui/api/editor/pages/DocumentationActivityExplorerPage.java
+++ plugins/org.eclipse.amalgam.explorer.activity.ui/src/org/eclipse/amalgam/explorer/activity/ui/api/editor/pages/DocumentationActivityExplorerPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c)  2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c)  2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,20 +13,17 @@ package org.eclipse.amalgam.explorer.activity.ui.api.editor.pages;
 import org.eclipse.ui.forms.editor.FormEditor;
 
 /**
- * Documentation that allows end-user to enter HTML documentation related to the model.
- * 
+ * Documentation that allows end-user to enter HTML documentation related to the
+ * model.
  */
-public class DocumentationActivityExplorerPage extends CommonActivityExplorerPage
-// implements IRichTextSaveable
-{
+public class DocumentationActivityExplorerPage extends CommonActivityExplorerPage {
 
-  public DocumentationActivityExplorerPage(FormEditor editor) {
-    super(editor, DocumentationActivityExplorerPage.class.getName(), Messages.DocumentationActivityExplorerPage_0);
-    // TODO Auto-generated constructor stub
-  }
+    public DocumentationActivityExplorerPage(FormEditor editor) {
+        super(editor, DocumentationActivityExplorerPage.class.getName(), Messages.DocumentationActivityExplorerPage_0);
+    }
 
-  @Override
-  public boolean isVisible() {
-    return true;
-  }
+    @Override
+    public boolean isVisible() {
+        return true;
+    }
 }
