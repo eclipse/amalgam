c5d0c7dac0b9233666d74511861d1c73f0a4bde2
Pierre-Charles David <pierre-charles.david@obeo.fr>
2017-08-08 18:15:50 +0200

    [509735] Cleanup ExplorerActivity class
    Bug: 509735
    Change-Id: I458b4ad42df4e9714b6e216c8f0b797c9930e1ca
    Signed-off-by: Pierre-Charles David <pierre-charles.david@obeo.fr>

--- plugins/org.eclipse.amalgam.explorer.activity.ui/src/org/eclipse/amalgam/explorer/activity/ui/api/editor/activities/ExplorerActivity.java
+++ plugins/org.eclipse.amalgam.explorer.activity.ui/src/org/eclipse/amalgam/explorer/activity/ui/api/editor/activities/ExplorerActivity.java
@@ -13,94 +13,95 @@ package org.eclipse.amalgam.explorer.activity.ui.api.editor.activities;
 import org.eclipse.amalgam.explorer.activity.ui.ActivityExplorerActivator;
 import org.eclipse.amalgam.explorer.activity.ui.api.configuration.ActivityConfiguration;
 import org.eclipse.amalgam.explorer.activity.ui.api.editor.pages.helper.FormHelper;
-import org.eclipse.amalgam.explorer.activity.ui.api.editor.predicates.IPredicate;
 import org.eclipse.amalgam.explorer.activity.ui.internal.intf.IOrdered;
 import org.eclipse.amalgam.explorer.activity.ui.internal.intf.IVisibility;
-import org.eclipse.swt.graphics.Image;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.forms.events.IHyperlinkListener;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ImageHyperlink;
 
+/**
+ * Represents an activity than can be invoked by the end-user by clicking on the
+ * corresponding hyperlink.
+ */
 public class ExplorerActivity implements IVisibility, IOrdered {
-    private ActivityConfiguration config;
+    /**
+     * The configuration for this activity.
+     */
+    private final ActivityConfiguration config;
+
+    /**
+     * The preference store where to look for visibility configuration (can be
+     * changed the the end-user).
+     */
+    private final IPreferenceStore preferenceStore = ActivityExplorerActivator.getDefault().getPreferenceStore();
+
+    /**
+     * The actual widget which represents the activity inside a section.
+     */
     private ImageHyperlink widget;
 
-	public ExplorerActivity(ActivityConfiguration cfg) {
-	    this.config = cfg;
-	}
-
-	public ExplorerActivity(String id, String name, IHyperlinkListener listener, IPredicate predicate, int index) {
-	    this.config = new ActivityConfiguration();
-		this.config.setId(id);
-		this.config.setName(name);
-		this.config.setListener(listener);
-		this.config.setPredicate(predicate);
-		this.config.setIndex(index);
-	}
-
-	public String getId() {
-		return config.getId();
-	}
-
-	public String getName() {
-		return config.getName();
-	}
-
-	public IHyperlinkListener getListener() {
-		return config.getListener();
-	}
-
-	public Image getImage() {
-		return config.getImage();
-	}
-
-	public String getDescription() {
-		return config.getDescription();
-	}
-
-	/**
-	 * Return true this activity is visible.
-	 */
-	public boolean isVisible() {
-		boolean result = ActivityExplorerActivator.getDefault().getPreferenceStore().getBoolean(getId());
-		if (config.getPredicate() != null) {
-			result &= config.getPredicate().isOk();
-		}
-		return result;
-	}
-
-	public IPredicate getPredicate() {
-		return config.getPredicate();
-	}
-
-	public int getPosition() {
-		return config.getIndex();
-	}
-
-	public void setPosition(int index_p) {
-		this.config.setIndex(index_p);
-	}
-
-    public int compareTo(IOrdered arg0) {
-        return new Integer(getPosition()).compareTo(new Integer(arg0.getPosition()));
+    public ExplorerActivity(ActivityConfiguration cfg) {
+        this.config = cfg;
     }
-    
-	public Control init(Composite activityContainer_p, FormToolkit toolkit_p) {
-		widget = FormHelper.createLinkWithDescription(toolkit_p, activityContainer_p, config.getImage(), config.getName(), null, config.getDescription(),
-		        config.getListener());
-		return widget;
-	}
 
-	public ImageHyperlink getWidget() {
-		return widget;
-	}
+    public String getId() {
+        return config.getId();
+    }
 
-	public void dispose() {
-		// dispose the section widget
-		if (widget != null && !widget.isDisposed())
-			widget.dispose();
-	}
+    public String getName() {
+        return config.getName();
+    }
+
+    public IHyperlinkListener getListener() {
+        return config.getListener();
+    }
+
+    @Override
+    public int getPosition() {
+        return config.getIndex();
+    }
+
+    @Override
+    public void setPosition(int index) {
+        this.config.setIndex(index);
+    }
+
+    @Override
+    public boolean isVisible() {
+        boolean result = preferenceStore.getBoolean(config.getId());
+        if (config.getPredicate() != null) {
+            result &= config.getPredicate().isOk();
+        }
+        return result;
+    }
+
+    @Override
+    public int compareTo(IOrdered other) {
+        return this.getPosition() - other.getPosition();
+    }
+
+    /**
+     * Create the actual hyperlink widget representing the activity.
+     * 
+     * @param parent
+     *            the parent control.
+     * @param toolkit
+     *            the toolkit to use for widget creation & styling.
+     * @return the hyperlink widget representing the activity.
+     */
+    public Control init(Composite parent, FormToolkit toolkit) {
+        widget = FormHelper.createLinkWithDescription(toolkit, parent, config.getImage(), config.getName(), null, config.getDescription(), config.getListener());
+        return widget;
+    }
+
+    public void dispose() {
+        // dispose the section widget
+        if (widget != null && !widget.isDisposed()) {
+            widget.dispose();
+        }
+    }
 
 }
